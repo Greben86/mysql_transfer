2f99a0611f307f6441f5f4d20b36a0444eca939f
Victor Greben <grebenvictor@yandex.ru>
2017-04-18 15:35:17 +0300

    refactoring

--- reader.js
+++ reader.js
@@ -3,17 +3,16 @@ var mysql  = require('mysql');
 var saver = require('./saver.js');
 var connection;
 
-
 function start(config) {	
 	connection = mysql.createConnection(config.database.mysql);
 
 	connection.connect();
-	readUsers(function(){
+	readUsers(config, function(){
 		connection.end();
 	});
 }
 
-function readUsers(callback) {	
+function readUsers(config, callback) {	
 	connection.query('SELECT `id`, `username`, `fullname`, `email`, `active` FROM `sn_user`', [], function(err, result) {
 		
 		if (err)
@@ -21,13 +20,13 @@ function readUsers(callback) {
 
 		console.log();
 		console.log(' (i) Read users is ok. Count users: ' + result.length);
-		saver.insertUsers(result, function(){
-			readMarks(callback);
+		saver.insertUsers(config, result, function(){
+			readMarks(config, callback);
 		});		
 	});
 }
 
-function readMarks(callback) {
+function readMarks(config, callback) {
 	connection.query('SELECT `id`, `value` FROM `sn_type_mark`', [], function(err, result) {
 		
 		if (err)
@@ -35,13 +34,13 @@ function readMarks(callback) {
 		
 		console.log();
 		console.log(' (i) Read marks is ok. Count types: ' + result.length);
-		saver.insertMarks(result, function(){
-			readTypes(callback);
+		saver.insertMarks(config, result, function(){
+			readTypes(config, callback);
 		});
 	});
 }
 
-function readTypes(callback) {
+function readTypes(config, callback) {
 	connection.query('SELECT `id`, `value` FROM `sn_type_org`', [], function(err, result) {
 		
 		if (err)
@@ -49,53 +48,53 @@ function readTypes(callback) {
 		
 		console.log();
 		console.log(' (i) Read types is ok. Count types: ' + result.length);
-		saver.insertTypes(result, function(){
-			readClients(callback);
+		saver.insertTypes(config, result, function(){
+			readClients(config, callback);
 		});
 	});
 }
 
-function readClients(callback){
+function readClients(config, callback){
 	connection.query(
 		'SELECT c.`id`, c.`org`, c.`id_region`, c.`comment`, c.`manager`, c.`site`, c.`inn`, c.`city`, t.`value` AS `typeorg`, m.`value` AS typemark, 0 AS `region` '+
 		'FROM `sn_client` AS c '+
 		'  LEFT JOIN `sn_type_org` AS t ON (t.`id`=c.`id_typeorg`) '+
-		'  LEFT JOIN `sn_type_mark` AS m ON (m.`id`=c.`mark`)', [], function(err, result) {
+		'  LEFT JOIN `sn_type_mark` AS m ON (m.`id`=c.`mark`) limit 1000', [], function(err, result) {
 		
 		if (err)
 			throw err;
 
 		console.log();
 		console.log(' (i) Read clients is ok. Count clients: ' + result.length);
-		saver.insertClients(result, function(){
-			readContacts(callback);
+		saver.insertClients(config, result, function(){
+			readContacts(config, callback);
 		});		
 	});
 }
 
-function readContacts(callback){
-	connection.query('SELECT `id`, `id_client`, `name`, `email`, `phone` FROM `sn_contact`', [], function(err, result) {
+function readContacts(config, callback){
+	connection.query('SELECT `id`, `id_client`, `name`, `email`, `phone` FROM `sn_contact` limit 1000', [], function(err, result) {
 	
 		if (err)
 			throw err;
 
 		console.log();
 		console.log(' (i) Read contacts is ok. Count contacts: ' + result.length);
-		saver.insertContacts(result, function(){
-			readTasks(callback);
+		saver.insertContacts(config, result, function(){
+			readTasks(config, callback);
 		});		
 	});
 }
 
-function readTasks(callback){
-	connection.query('SELECT `date`, `type`, `text`, `status`, `user`, `id_client` FROM `sn_task`', [], function(err, result) {
+function readTasks(config, callback){
+	connection.query('SELECT `date`, `type`, `text`, `status`, `user`, `id_client` FROM `sn_task` limit 1000', [], function(err, result) {
 
 		if (err)
 			throw err;
 
 		console.log();
 		console.log(' (i) Read tasks is ok. Count tasks: ' + result.length);
-		saver.insertTasks(result, function(){
+		saver.insertTasks(config, result, function(){
 			callback();
 		});
 	});
--- saver.js
+++ saver.js
@@ -1,5 +1,3 @@
-var readConfig = require('read-config');
-    config = readConfig('./config.json');
 var Firebird = require('node-firebird');
 
 function ActiveToSwitchOff(value) {
@@ -15,215 +13,180 @@ function ActiveToSwitchOff(value) {
 	}
 }
 
-function recursiveInsertUsers(users, index, callback){
-	if (index === 0){
+function recursiveInsertUsers(db, users, index, callback){
+	if (index < 0){
 		callback();
 		return;
 	}
 
-	Firebird.attach(config.database.firebird, function(err, db) {
-		if (err)
-			throw err;
-
-		db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
-			transaction.query(
-			"UPDATE OR INSERT INTO users (id_ext, login, name, workemail, switchoff, pass, rights, dismiss) "+
-			"VALUES (?, ?, ?, ?, ?, ?, 3, 'F') "+
-			"MATCHING (id_ext)", 
-			[users[index].id, 
-			(users[index].username+"").substr(0, 15), 
-			(users[index].fullname+"").substr(0, 30), 
-			(users[index].email+"").substr(0, 30),
-			ActiveToSwitchOff(users[index].active),
-			"930023ae91342d9ac1dd8d75eeebb844"], 
-			function(err, result) {
-				if (err) {
-					console.error(err);
-					transaction.rollback();
-					db.detach();
-				} else {						
-					transaction.commit(function(err) {
-						if (err) {
-							console.error(err);
-							transaction.rollback();
-						} else {
-							console.log(users[index].username + ' -> save user is Ok');
-							recursiveInsertUsers(users, index-1, callback);
-						}											
-						db.detach();
-					});
-				}
-			});
+	db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
+		transaction.query(
+		"UPDATE OR INSERT INTO users (id_ext, login, name, workemail, switchoff, pass, rights, dismiss) "+
+		"VALUES (?, ?, ?, ?, ?, ?, 3, 'F') "+
+		"MATCHING (id_ext)", 
+		[users[index].id, 
+		(users[index].username+"").substr(0, 15), 
+		(users[index].fullname+"").substr(0, 30), 
+		(users[index].email+"").substr(0, 30),
+		ActiveToSwitchOff(users[index].active),
+		"930023ae91342d9ac1dd8d75eeebb844"], 
+		function(err, result) {
+			if (err) {
+				console.error(err);
+				transaction.rollback();
+			} else {						
+				transaction.commit(function(err) {
+					if (err) {
+						console.error(err);
+						transaction.rollback();
+					} else {
+						console.log("[" + index + "] " + users[index].username + ' -> save user is Ok');
+					}											
+					recursiveInsertUsers(db, users, index-1, callback);
+				});
+			}
 		});
 	});
 }
 
-function recursiveInsertMarks(marks, index, callback){
-	if (index === 0){
+function recursiveInsertMarks(config, db, marks, index, callback){
+	if (index < 0){
 		callback();
 		return;
 	}
 
-	Firebird.attach(config.database.firebird, function(err, db) {
-		if (err)
-			throw err;
-
-		db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
-			transaction.query(
-			"UPDATE OR INSERT INTO "+config.tables.marks+" (name, folder, owner) VALUES (?, 1, 0) MATCHING (name)", 
-			[marks[index].value], 
-			function(err, result) {
-				if (err) {
-					console.error(err);
-					transaction.rollback();
-					db.detach();
-				} else {						
-					transaction.commit(function(err) {
-						if (err) {
-							console.error(err);
-							transaction.rollback();
-						} else {
-							console.log(marks[index].value + ' -> save mark is Ok');
-							recursiveInsertMarks(marks, index-1, callback);
-						}											
-						db.detach();
-					});
-				}
-			});
+	db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
+		transaction.query(
+		"UPDATE OR INSERT INTO "+config.tables.marks+" (name, folder, owner) VALUES (?, 1, 0) MATCHING (name)", 
+		[marks[index].value], 
+		function(err, result) {
+			if (err) {
+				console.error(err);
+				transaction.rollback();
+			} else {						
+				transaction.commit(function(err) {
+					if (err) {
+						console.error(err);
+						transaction.rollback();
+					} else {
+						console.log("[" + index + "] " + marks[index].value + ' -> save mark is Ok');
+					}											
+					recursiveInsertMarks(config, db, marks, index-1, callback);
+				});
+			}
 		});
 	});
 }
 
-function recursiveInsertTypes(types, index, callback){
-	if (index === 0){
+function recursiveInsertTypes(config, db, types, index, callback){
+	if (index < 0){
 		callback();
 		return;
 	}
 
-	Firebird.attach(config.database.firebird, function(err, db) {
-		if (err)
-			throw err;
-
-		db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
-			transaction.query(
-			"UPDATE OR INSERT INTO "+config.tables.types+" (name, folder, owner) VALUES (?, 1, 0) MATCHING (name)", 
-			[types[index].value], 
-			function(err, result) {
-				if (err) {
-					console.error(err);
-					transaction.rollback();
-					db.detach();
-				} else {						
-					transaction.commit(function(err) {
-						if (err) {
-							console.error(err);
-							transaction.rollback();
-						} else {
-							console.log(types[index].value + ' -> save type is Ok');
-							recursiveInsertTypes(types, index-1, callback);
-						}											
-						db.detach();
-					});
-				}
-			});
+	db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
+		transaction.query(
+		"UPDATE OR INSERT INTO "+config.tables.types+" (name, folder, owner) VALUES (?, 1, 0) MATCHING (name)", 
+		[types[index].value], 
+		function(err, result) {
+			if (err) {
+				console.error(err);
+				transaction.rollback();
+			} else {						
+				transaction.commit(function(err) {
+					if (err) {
+						console.error(err);
+						transaction.rollback();
+					} else {
+						console.log("[" + index + "] " + types[index].value + ' -> save type is Ok');
+					}											
+					recursiveInsertTypes(config, db, types, index-1, callback);
+				});
+			}
 		});
 	});
 }
 
-function recursiveInsertClients(clients, index, callback){
-	if (index === 0) {
+function recursiveInsertClients(config, db, clients, index, callback){
+	if (index < 0) {
 		callback();
 		return;
 	}
 
-	Firebird.attach(config.database.firebird, function(err, db) {
-		if (err)
-			throw err;
-
-		db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
-			transaction.query(
-			"UPDATE OR INSERT INTO customer ("+
-			"  id_ext, name, fullname, prim, website, inn, curator, specialist, region, city, "+config.tables.types+", "+config.tables.marks+", "+
-			"  custtype, folder, owner, regdate, actual_time, privateclient"+
-			") VALUES ("+
-			"  ?, ?, ?, ?, ?, ?, (select first(1) id from users where id_ext=?), "+
-			"  (select first(1) id from users where id_ext=?), (select first(1) id from places where name=?), "+
-			"  (select first(1) id from places where name=?), "+
-			"  (select first(1) id from "+config.tables.types+" where name=?), (select first(1) id from "+config.tables.marks+" where name=?), "+
-			"  1, 1, 0, current_timestamp, current_timestamp, 'F'"+
-			") MATCHING (id_ext)", 
-			[clients[index].id,
-			(clients[index].org+"").substr(0, 200),
-			(clients[index].org+"").substr(0, 200),
-			clients[index].comment,
-			(clients[index].site+"").substr(0, 50),
-			(clients[index].inn+"").substr(0, 30),
-			clients[index].manager,
-			clients[index].manager,
-			clients[index].region,
-			clients[index].city,
-			clients[index].typeorg,
-			clients[index].typemark],
-			function(err, result) {
-				if (err) {
-					console.error(err);
-					transaction.rollback();
-					db.detach();
-				} else {						
-					transaction.commit(function(err) {
-						if (err) {
-							console.error(err);
-							transaction.rollback();
-						} else {
-							console.log(clients[index].org + ' -> save client is Ok');
-							recursiveInsertClients(clients, index-1, callback);
-						}											
-						db.detach();
-					});
-				}
-			});
+	db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
+		transaction.query(
+		"UPDATE OR INSERT INTO customer ("+
+		"  id_ext, name, fullname, prim, website, inn, curator, specialist, region, city, "+config.tables.types+", "+config.tables.marks+", "+
+		"  custtype, folder, owner, regdate, actual_time, privateclient"+
+		") VALUES ("+
+		"  ?, ?, ?, ?, ?, ?, (select first(1) id from users where id_ext=?), "+
+		"  (select first(1) id from users where id_ext=?), (select first(1) id from places where name=?), "+
+		"  (select first(1) id from places where name=?), "+
+		"  (select first(1) id from "+config.tables.types+" where name=?), (select first(1) id from "+config.tables.marks+" where name=?), "+
+		"  1, 1, 0, current_timestamp, current_timestamp, 'F'"+
+		") MATCHING (id_ext)", 
+		[clients[index].id,
+		(clients[index].org+"").substr(0, 200),
+		(clients[index].org+"").substr(0, 200),
+		clients[index].comment,
+		(clients[index].site+"").substr(0, 50),
+		(clients[index].inn+"").substr(0, 30),
+		clients[index].manager,
+		clients[index].manager,
+		clients[index].region,
+		clients[index].city,
+		clients[index].typeorg,
+		clients[index].typemark],
+		function(err, result) {
+			if (err) {
+				console.error(err);
+				transaction.rollback();
+			} else {						
+				transaction.commit(function(err) {
+					if (err) {
+						console.error(err);
+						transaction.rollback();
+					} else {
+						console.log("[" + index + "] " + clients[index].org + ' -> save client is Ok');						
+					}											
+					recursiveInsertClients(config, db, clients, index-1, callback);
+				});
+			}
 		});
 	});
 }
 
-function recursiveInsertContacts(contacts, index, callback){
-	if (index === 0){
+function recursiveInsertContacts(db, contacts, index, callback){
+	if (index < 0){
 		callback();
 		return;
 	}
 
-	Firebird.attach(config.database.firebird, function(err, db) {
-		if (err)
-			throw err;
-
-		db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
-			transaction.query(
-			"UPDATE OR INSERT INTO contactpersons (id_ext, name, email, phone, custno) "+
-			"VALUES (?, ?, ?, ?, (select first(1) id from customer where id_ext=?)) "+
-			"MATCHING (id_ext)", 
-			[contacts[index].id, 
-			(contacts[index].name+"").substr(0, 30), 
-			(contacts[index].email+"").substr(0, 50),
-			(contacts[index].phone+"").substr(0, 50), 
-			contacts[index].id_client], 
-			function(err, result) {
-				if (err) {
-					console.error(err);
-					transaction.rollback();
-					db.detach();
-				} else {						
-					transaction.commit(function(err) {
-						if (err) {
-							console.error(err);
-							transaction.rollback();
-						} else {
-							console.log(contacts[index].name + ' -> save contact is Ok');
-							recursiveInsertContacts(contacts, index-1, callback);
-						}											
-						db.detach();
-					});
-				}
-			});
+	db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
+		transaction.query(
+		"UPDATE OR INSERT INTO contactpersons (id_ext, name, email, phone, custno) "+
+		"VALUES (?, ?, ?, ?, (select first(1) id from customer where id_ext=?)) "+
+		"MATCHING (id_ext)", 
+		[contacts[index].id, 
+		(contacts[index].name+"").substr(0, 30), 
+		(contacts[index].email+"").substr(0, 50),
+		(contacts[index].phone+"").substr(0, 50), 
+		contacts[index].id_client], 
+		function(err, result) {
+			if (err) {
+				console.error(err);
+				transaction.rollback();
+			} else {						
+				transaction.commit(function(err) {
+					if (err) {
+						console.error(err);
+						transaction.rollback();
+					} else {
+						console.log("[" + index + "] " + contacts[index].name + ' -> save contact is Ok');						
+					}
+					recursiveInsertContacts(db, contacts, index-1, callback);
+				});
+			}
 		});
 	});
 }
@@ -273,96 +236,125 @@ function TypeTask(value) {
 	}
 }
 
-function recursiveInsertTasks(tasks, index, callback){
-	if (index === 0){
+function recursiveInsertTasks(db, tasks, index, callback){
+	if (index < 0){
 		callback();
 		return;
 	}
 
-	Firebird.attach(config.database.firebird, function(err, db) {
-		if (err)
-			throw err;
-
-		db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
-			transaction.query(
-			"INSERT INTO task ("+
-			"  dateini, datestart, dateexp, description, taskstatus, maker, executor, custno, "+
-			"  hint, showntask, rush, control, done, remake, rem_need, rem_before, rem_time"+
-			") VALUES ("+
-			"  CAST(? AS timestamp), CAST(? AS timestamp), DATEADD(15 minute to CAST(? AS timestamp)), ?, ?, "+
-			"  (select first(1) id from users where id_ext=?), (select first(1) id from users where id_ext=?), "+
-			"  (select first(1) id from customer where id_ext=?), 'F', 'F', 3, 'F', ?, 'F', 'T', 15, DATEADD(-15 minute to CAST(? AS timestamp))) ", 
-			[tasks[index].date, 
-			tasks[index].date, 
-			tasks[index].date,
-			TypeTask(tasks[index].type) + tasks[index].text,
-			StatusTask(tasks[index].status),
-			tasks[index].user,
-			tasks[index].user,
-			tasks[index].id_client,
-			DoneTask(tasks[index].status), 
-			tasks[index].date], 
-			function(err, result) {
-				if (err) {
-					console.error(err);
-					transaction.rollback();
-					db.detach();
-				} else {						
-					transaction.commit(function(err) {
-						if (err) {
-							console.error(err);
-							transaction.rollback();
-						} else {
-							console.log(tasks[index].date + ' -> save task is Ok');
-							recursiveInsertTasks(tasks, index-1, callback);
-						}											
-						db.detach();
-					});
-				}
-			});
+	db.transaction(Firebird.ISOLATION_READ_COMMITED, function(err, transaction) {								
+		transaction.query(
+		"INSERT INTO task ("+
+		"  dateini, datestart, dateexp, description, taskstatus, maker, executor, custno, "+
+		"  hint, showntask, rush, control, done, remake, rem_need, rem_before, rem_time"+
+		") VALUES ("+
+		"  CAST(? AS timestamp), CAST(? AS timestamp), DATEADD(15 minute to CAST(? AS timestamp)), ?, ?, "+
+		"  (select first(1) id from users where id_ext=?), (select first(1) id from users where id_ext=?), "+
+		"  (select first(1) id from customer where id_ext=?), 'F', 'F', 3, 'F', ?, 'F', 'T', 15, DATEADD(-15 minute to CAST(? AS timestamp))) ", 
+		[tasks[index].date, 
+		tasks[index].date, 
+		tasks[index].date,
+		TypeTask(tasks[index].type) + tasks[index].text,
+		StatusTask(tasks[index].status),
+		tasks[index].user,
+		tasks[index].user,
+		tasks[index].id_client,
+		DoneTask(tasks[index].status), 
+		tasks[index].date], 
+		function(err, result) {
+			if (err) {
+				console.error(err);
+				transaction.rollback();
+			} else {						
+				transaction.commit(function(err) {
+					if (err) {
+						console.error(err);
+						transaction.rollback();
+					} else {
+						console.log("[" + index + "] " + tasks[index].date + ' -> save task is Ok');
+					}
+					recursiveInsertTasks(db, tasks, index-1, callback);
+				});
+			}
 		});
 	});
 }
 
-function insertUsers(users, callback){
-	recursiveInsertUsers(users, users.length-1, function(){
-		console.log(' (i) Users inserted.');
-		callback();
+function insertUsers(config, users, callback){
+	Firebird.attach(config.database.firebird, function(err, db) {
+		if (err)
+			throw err;
+
+		recursiveInsertUsers(db, users, users.length-1, function(){
+			console.log(' (i) Users inserted.');
+			db.detach();
+			callback();
+		});	
+	});	
+}
+
+function insertMarks(config, marks, callback){
+	Firebird.attach(config.database.firebird, function(err, db) {
+		if (err)
+			throw err;
+
+		recursiveInsertMarks(config, db, marks, marks.length-1, function(){
+			console.log(' (i) Marks inserted.');
+			db.detach();
+			callback();
+		});	
+	});	
+}
+
+function insertTypes(config, types, callback){
+	Firebird.attach(config.database.firebird, function(err, db) {
+		if (err)
+			throw err;
+
+		recursiveInsertTypes(config, db, types, types.length-1, function(){
+			console.log(' (i) Types inserted.');
+			db.detach();
+			callback();
+		});	
 	});
 }
 
-function insertClients(clients, callback){
-	recursiveInsertClients(clients, clients.length-1, function(){
-		console.log(' (i) Clients inserted.');
-		callback();
+function insertClients(config, clients, callback){
+	Firebird.attach(config.database.firebird, function(err, db) {
+		if (err)
+			throw err;
+
+		recursiveInsertClients(config, db, clients, clients.length-1, function(){
+			console.log(' (i) Clients inserted.');
+			db.detach();
+			callback();
+		});	
 	});
 }
 
-function insertMarks(marks, callback){
-	recursiveInsertMarks(marks, marks.length-1, function(){
-		console.log(' (i) Marks inserted.');
-		callback();
+function insertContacts(config, contacts, callback){
+	Firebird.attach(config.database.firebird, function(err, db) {
+		if (err)
+			throw err;
+
+		recursiveInsertContacts(db, contacts, contacts.length-1, function(){
+			console.log(' (i) Contacts inserted.');
+			db.detach();
+			callback();
+		});	
 	});
 }
 
-function insertTypes(types, callback){
-	recursiveInsertTypes(types, types.length-1, function(){
-		console.log(' (i) Types inserted.');
-		callback();
-	});
-}
+function insertTasks(config, tasks, callback){
+	Firebird.attach(config.database.firebird, function(err, db) {
+		if (err)
+			throw err;
 
-function insertContacts(contacts, callback){
-	recursiveInsertContacts(contacts, contacts.length-1, function(){
-		console.log(' (i) Contacts inserted.');
-		callback();
-	});
-}
-
-function insertTasks(tasks, callback){
-	recursiveInsertTasks(tasks, tasks.length-1, function(){
-		console.log(' (i) Tasks inserted.');
-		callback();
+		recursiveInsertTasks(db, tasks, tasks.length-1, function(){
+			console.log(' (i) Tasks inserted.');
+			db.detach();
+			callback();
+		});	
 	});
 }
 
